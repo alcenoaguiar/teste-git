cca4766c2e79c18558b86afcfc27b31b8946a180
alcenoaguiar <alcenno34@hotmail.com>
2022-02-14 12:36:02 -0400

    commit teste

---
+++ indexx.js
@@ -0,0 +1,21 @@
+console.log ("eu funciono"):
+console.log("arquivo de scrip externo"):
+let meuNumero = 20;
+let outroNumero = 177;
+
+console.log(meuNumero + outroNumero);
+
+let meuNome = 'alceno aguiar'
+
+length ComprimentoMeuNome 
+
+CSSCounterStyleRule.log('o meu nome possui.. $')
+
+
+/*
+>    maior que
+<    menor que
+>=   maior ou igual
+<=    =menor ou igual
+ 
+*/
---
+++ script.js
@@ -0,0 +1,33 @@
+//variavel
+/*String '2' ou " "
+*number 1 2 
+
+*atribuir valor =
+*coparar valor ==
+*identico, compara valor e tipo ===
+*
+
+*
+* &&    and  -  ambas as condiçoes precisam retornar TRUE/ precisa ser verdadeiro
+* ||    or   -   ao menos uma precisa ser TRUE
+* !     not -  as duas condições devem ser FALSE
+!==   not   diferente
+>    maior que
+<    menor que
+>=   maior ou igual
+<=    =menor ou igual
+ 
+*/
+
+/* + =soma
+* - =subtração
+* * =multiplicação
+* / =divisão
+* % =mod/modulo = resto da divisão
+* ++ =incremento   3++ = 3+1
+* -- =descremento  2-- = 2-1
+*/
+
+function nomeDaFuncao() {
+
+}
